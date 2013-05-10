d763cfcebf618afd18619f6745b4b7a6b2b6ef2b
Chris Nuernberger <cnuernber@gmail.com>
2013-05-10 08:07:49 -0600

    Adding eastl and getting ready to do actual work

---
+++ cclj/cclj/ccljgc.h
@@ -0,0 +1,7 @@
+//==============================================================================
+//  Copyright 2013, Chris Nuernberger
+//	ALL RIGHTS RESERVED
+//
+//  This code is licensed under the BSD license located in the top.  Terms of the
+//	license are located under the top cclj directory
+//==============================================================================
